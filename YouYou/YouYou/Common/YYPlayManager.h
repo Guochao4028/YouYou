84b2e6f7e851134b5b7b93af12d2703567788e66
guochao <chao.guo@aiyingli.com>
2019-01-16 14:31:00 +0800

    播放音频类

---
+++ YouYou/YouYou/Common/YYPlayManager.h
@@ -0,0 +1,17 @@
+//
+//  YYPlayManager.h
+//  YouYou
+//
+//  Created by AYLiOS on 2019/1/16.
+//  Copyright © 2019 AYLiOS. All rights reserved.
+//  播放音频类
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface YYPlayManager : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ YouYou/YouYou/Common/YYPlayManager.m
@@ -0,0 +1,13 @@
+//
+//  YYPlayManager.m
+//  YouYou
+//
+//  Created by AYLiOS on 2019/1/16.
+//  Copyright © 2019 AYLiOS. All rights reserved.
+//
+
+#import "YYPlayManager.h"
+
+@implementation YYPlayManager
+
+@end
